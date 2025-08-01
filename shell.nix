5f5e281ebf18983f4c5f7f366fd0658653bc6e6b
Roman Jost <romanjost1@proton.me>
2025-08-01 19:01:07 +0200

    chore(nix-shell): updated shell to make it possible to build

--- shell.nix
+++ shell.nix
@@ -2,15 +2,32 @@ with import <nixpkgs> {};
 
 mkShell {
   buildInputs = [
+    rustup
     cargo
     openssl
     pkg-config
     rust-analyzer
-    rustc
-    rustfmt
   ];
 
+  shellHook = ''
+    export RUSTUP_HOME="$HOME/.rustup"
+    export CARGO_HOME="$HOME/.cargo"
+    export PATH="$CARGO_HOME/bin:$PATH"
+    
+    # Installiere Nightly falls nicht vorhanden und setze es als Standard
+    if ! rustup show | grep -q "nightly"; then
+      rustup install nightly
+    fi
+    rustup default nightly
+  '';
 
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-  RUSTC_BOOTSTRAP = "1";
+  # Optional: Toolchain explizit setzen
+  RUSTUP_TOOLCHAIN = "nightly";
+
+  # OpenSSL Konfiguration
+  OPENSSL_DIR = "${pkgs.openssl.dev}";
+  OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+  OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 }
+
